d79af9ca19941625da0be38d7f8ad5c858d1d34b
Taisuke Fukuno <fukuno@jig.jp>
2022-08-31 18:49:20 +0900

    make es module

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: "production",
+    entry: "index.js",
+    output: {
+        filename: "HTMLParser.js",
+        library: "HTMLParser",
+        libraryTarget: "var"
+    },
+}
